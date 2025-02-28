c043e2a2f2e7eaaef7f82345c9d25a5ac85d1d87
garcia2304 <garcia2304@icloud.com>
2025-02-28 09:25:22 -0500

    more fixed bugs

--- pages/api/getRecommendation.js
+++ pages/api/getRecommendation.js
@@ -13,6 +13,5 @@ export default async function handler(req, res) {
       return res.status(200).json(productRecommendation);
     } catch (error) {
       console.error("Error in getRecommendation API:", error);
-      return res.status(500).json({ error: error.message });
     }
   }
